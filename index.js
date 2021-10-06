030da0872fa5380dc8594a0a42831bd0cd34c571
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-06 19:55:42 -0300

    Modificação da extensão do arquivo de configuração de js para yml e criação do controlador de processos

--- index.js
+++ index.js
@@ -20,17 +20,15 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3001');
 });
