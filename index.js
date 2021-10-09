a110e007c61d119f4a7e2292eb1a51d45216ec89
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-10-09 20:07:41 -0300

    Configurando deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -8,25 +10,25 @@ const StrangerThingsService = require('./services/StrangerThings');
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+    strangerThingsDataset,
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+    strangerThingsRepository,
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
+    const characters = strangerThingsService.search(
+        req.query,
+        hereIsTheUpsideDown,
+    );
 
-  res.status(200).json(characters);
+    res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Escutando na porta 3000');
 });
