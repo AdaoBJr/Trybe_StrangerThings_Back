3298c2f2f606b30d30b633dddcf690dc0aa381c5
Abimaelr <abimaelr.albu@gmail.com>
2021-10-10 12:39:31 -0300

    variaveis de ambiente

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: pm2-runtime start ecosystem.config.yml --env production
+web: pm2-runtime start ecosystem.config.yml
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 // starting
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,17 +17,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-// const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    process.env.UPSIDEDOWN_MODE,
+    hereIsTheUpsideDown,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Escutando na porta 3000');
 });
