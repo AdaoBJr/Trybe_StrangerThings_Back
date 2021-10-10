dd349762da15b038e41dffea67d8449af8f1a24b
Abimaelr <abimaelr.albu@gmail.com>
2021-10-10 12:49:54 -0300

    Variavel para PORT

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
-
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
