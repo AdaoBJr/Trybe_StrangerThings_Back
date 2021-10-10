66a4ae2f3992a4e556a9e3575821677751cff1d7
andredebem <andrebemferraz@gmail.com>
2021-10-10 16:04:01 -0300

    Request I

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// First commit
-
 const express = require('express');
 const cors = require('cors');
+
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
