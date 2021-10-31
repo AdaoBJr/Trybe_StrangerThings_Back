104967b1fa8de976190d9df5d94ebc5be746e8cb
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-31 12:17:26 -0300

    Tira o ou 3000 da const PORT para ver se desbuga o Heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env;
 
 const app = express();
 
