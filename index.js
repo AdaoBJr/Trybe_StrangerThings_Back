3c3a0db3aa322d3696d852c850ac3e79868fa142
walterlbf <walter.lbf.f@gmail.com>
2021-11-11 02:28:58 -0400

    sei la o que ta rolando

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE.toLowerCase() === 'true' || false;
+const { PORT } = process.env;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
