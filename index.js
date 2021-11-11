3cd4de6e58ea693c48ab45bd96354416ab4395ca
João Victor Pistorio <joao.pistorio@gmail.com>
2021-11-11 11:12:53 -0300

    Edit index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-
 require('dotenv').config();
 
-const { PORT, UPSIDEDOWN_MODE } = process.env;
+const express = require('express');
+const cors = require('cors');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -20,17 +18,19 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = JSON.parse(UPSIDEDOWN_MODE);
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
