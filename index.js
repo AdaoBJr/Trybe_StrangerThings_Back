92d73728c64587886010e1d7b55cb23383b3d4ac
João Victor Pistorio <joao.pistorio@gmail.com>
2021-11-11 10:39:14 -0300

    Edit app listen

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
